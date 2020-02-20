1b4165f95a69dad8e00ffb510c1b2b2ae23c485e
Dominik Harz <dominik.harz@gmail.com>
2020-02-20 22:47:57 +0900

    IndexMap

--- crates/bitcoin/src/lib.rs
+++ crates/bitcoin/src/lib.rs
@@ -7,9 +7,19 @@ use codec::{Encode, Decode};
 use primitive_types::{U256, H256};
 use node_primitives::{Moment};
 
+// IndexMap
+// use core::hash::BuildHasherDefault;
+// use indexmap::IndexMap;
+// use twox_hash::XxHash64;
+
 /// Custom Types
 /// Bitcoin Raw Block Header type
 pub type RawBlockHeader = RawHeader;
+
+// NOTE: This is a more efficient type for mappings (compared to BTreeMap),
+// but currently not supported by Substrate
+// /// A mapping type for blockchains
+// pub type Map<K, V> = IndexMap<K, V, BuildHasherDefault<XxHash64>>;
 
 /// Structs
 /// Bitcoin Block Headers
@@ -18,7 +28,7 @@ pub type RawBlockHeader = RawHeader;
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct BlockHeader<U256, H256, Timestamp> {
     pub block_hash: H256, 
-    pub block_height: U256,
+    pub block_height: Option<U256>,
     pub merkle_root: H256,
     pub target: U256,
     pub timestamp: Timestamp,
@@ -35,24 +45,24 @@ pub struct BlockHeader<U256, H256, Timestamp> {
 // TODO: ask if there is a "mapping" type in structs
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
-pub struct BlockChain<U256, H256> {
+pub struct BlockChain<U256, Map> {
     pub chain_id: U256,
-    pub chain: Vec<H256>,
+    pub chain: Map,
     pub max_height: U256,
     pub no_data: bool,
     pub invalid: bool,
 }
 
 
-pub fn parse_block_header(block_header_bytes: Vec<u8>, block_height: U256) -> BlockHeader<U256, H256, Moment> {
+pub fn parse_block_header(block_header_bytes: Vec<u8>) -> BlockHeader<U256, H256, Moment> {
     let merkle_root: H256 = H256::zero();
-    let timestamp: Moment = 0; 
+    let timestamp: Moment = Moment::default(); 
     let target: U256 = U256::max_value();
     let hash_current_block: H256 = H256::zero();
     // returns a new BlockHeader struct
     let block_header = BlockHeader {
         block_hash: hash_current_block,
-        block_height: block_height,
+        block_height: None,
         merkle_root: merkle_root,
         target: target,
         timestamp: timestamp,
@@ -64,7 +74,7 @@ pub fn parse_block_header(block_header_bytes: Vec<u8>, block_height: U256) -> Bl
         hash_prev_block: None
     };    
 
-    block_header
+    return block_header
 }
 
 
