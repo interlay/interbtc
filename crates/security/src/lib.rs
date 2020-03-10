46fe8edb72b9b998494d2c43866e80286cf00a03
alexei.zamyatin <alexei.zamyatin@gmail.com>
2020-03-10 16:56:12 +0100

    Data model and types for security module

--- crates/security/src/lib.rs
+++ crates/security/src/lib.rs
@@ -1,4 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+#[cfg(test)]
+mod tests; 
 
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch::DispatchResult, ensure};
 use system::ensure_signed;
@@ -16,11 +18,50 @@ pub trait Trait: system::Trait {
     /// Voter threshold
     const STAKED_RELAYER_VOTE_THRESHOLD: u8 = 0;
    
-    // /// Minimum stake
-    const MINIMUM_STAKE: Self::Currency;
+    // /// Minimum Staked Relayer stake
+	const STAKED_RELAYER_STAKE: Self::Currency;
+
 }
 
 pub type DOT<T> = <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
+
+// Indicated the status of the BTC Parachain.
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq)]
+enum StatusCode {
+	Running = 0,
+	Error = 1,
+	Shutdown = 2
+}
+
+// Enum specifying errors which lead to the Error status
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq)]
+pub enum ErrorCodes {
+    NoDataBTCRelay = 1,
+	InvalidBTCRelay = 2,
+	OracleOffline = 3,
+	Liquidation = 4
+}
+
+// Indicates the state of a proposed StatusUpdate.
+#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq)]
+pub enum ProposalStatus {
+	Pending = 0,
+	Accepted = 1, 
+	Rejected = 2
+}
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct StatusUpdate<StatusCode, ErrorCode, BlockNumber, AccountId> {
+	newStatusCode: StatusCode, 
+	oldStatusCode: StatusCode,
+	addErrors: HashSet<ErrorCode>,
+	removeErrors: HashSet<ErrorCode>,
+	time: BlockNumber, 
+	msg: String, 
+	votesYes: HashSet<AccountId>,
+	votesNo: HashSet<AccountId>
+}
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
@@ -28,16 +69,13 @@ pub struct StakedRelayer<Currency> {
       stake: Currency,
 }
 
-#[derive(Encode, Decode, Debug, Clone, PartialEq, Eq)]
-pub enum ErrorCodes {
-    NoDataBTCRelay,
-    InvalidBTCRelay,
-}
 
 // This pallet's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as SecurityModule {
-        StakedRelayers get(fn stakedrelayer): map T::AccountId => StakedRelayer<DOT<T>>; 
+
+		StakedRelayers get(fn stakedrelayer): map T::AccountId => StakedRelayer<DOT<T>>; 
+
 	}
 }
 
@@ -87,71 +125,4 @@ decl_error! {
         /// Insufficient stake provided
         InsufficientStake,
     }
-}
-
-/// tests for this pallet
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	use sp_core::H256;
-	use frame_support::{impl_outer_origin, assert_ok, parameter_types, weights::Weight};
-	use sp_runtime::{
-		traits::{BlakeTwo256, IdentityLookup}, testing::Header, Perbill,
-	};
-
-	impl_outer_origin! {
-		pub enum Origin for Test {}
-	}
-
-	// For testing the pallet, we construct most of a mock runtime. This means
-	// first constructing a configuration type (`Test`) which `impl`s each of the
-	// configuration traits of modules we want to use.
-	#[derive(Clone, Eq, PartialEq)]
-	pub struct Test;
-	parameter_types! {
-		pub const BlockHashCount: u64 = 250;
-		pub const MaximumBlockWeight: Weight = 1024;
-		pub const MaximumBlockLength: u32 = 2 * 1024;
-		pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
-	}
-	impl system::Trait for Test {
-		type Origin = Origin;
-		type Call = ();
-		type Index = u64;
-		type BlockNumber = u64;
-		type Hash = H256;
-		type Hashing = BlakeTwo256;
-		type AccountId = u64;
-		type Lookup = IdentityLookup<Self::AccountId>;
-		type Header = Header;
-		type Event = ();
-		type BlockHashCount = BlockHashCount;
-		type MaximumBlockWeight = MaximumBlockWeight;
-		type MaximumBlockLength = MaximumBlockLength;
-		type AvailableBlockRatio = AvailableBlockRatio;
-		type Version = ();
-		type ModuleToIndex = ();
-	}
-	impl Trait for Test {
-		type Event = ();
-	}
-	type TemplateModule = Module<Test>;
-
-	// This function basically just builds a genesis storage key/value store according to
-	// our desired mockup.
-	fn new_test_ext() -> sp_io::TestExternalities {
-		system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
-	}
-
-	#[test]
-	fn it_works_for_default_value() {
-		new_test_ext().execute_with(|| {
-			// Just a dummy test for the dummy funtion `do_something`
-			// calling the `do_something` function with a value 42
-			assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-			// asserting that the stored value is equal to what we stored
-			assert_eq!(TemplateModule::something(), Some(42));
-		});
-	}
 }
