bcb3bfd7a29773fd840498f72fdaf70df4eafd11
Dominik Harz <dominik@interlay.io>
2020-05-08 16:43:56 +0900

    include btc-relay test functions

--- parachain/runtime/tests/mock.rs
+++ parachain/runtime/tests/mock.rs
@@ -2,6 +2,7 @@ extern crate hex;
 
 pub use bitcoin::types::{BlockBuilder, TransactionBuilder, TransactionInputBuilder, TransactionOutput};
 pub use bitcoin::types::{H256Le, Address};
+pub use bitcoin::formatter::Formattable;
 pub use btc_parachain_runtime::{AccountId, Event, Runtime};
 pub use frame_support::{assert_err, assert_ok};
 pub use mocktopus::mocking::*;
@@ -13,6 +14,8 @@ pub use x_core::Error;
 
 pub const ALICE: [u8; 32] = [0u8; 32];
 pub const BOB: [u8; 32] = [1u8; 32];
+
+pub type BTCRelayCall = btc_relay::Call<Runtime>;
 
 pub fn origin_of(account_id: AccountId) -> <Runtime as system::Trait>::Origin {
     <Runtime as system::Trait>::Origin::signed(account_id)
@@ -59,7 +62,9 @@ pub fn generate_transaction_and_mine(
     dest_address: H160,
     amount: u128,
     return_data: H256
-) {
+) -> (H256Le, u32, Vec<u8>, Vec<u8>) {
+    let mut height = 1;
+    let confirmations = 6;
     // initialize BTC Relay with one block
     let init_block = BlockBuilder::new()
         .with_version(2)
@@ -68,10 +73,12 @@ pub fn generate_transaction_and_mine(
         .mine(U256::from(2).pow(254.into()));
 
     let init_block_hash = init_block.header.hash();
+    let raw_init_block_header = Formattable::format(&init_block.header);
 
-    // FIXME: need a function to convert a BlockHeader to a RawBlockHeader
-    btc_relay::Call<Runtime>::initialize(raw_init_block_header, 1)
-        .dispatch(origin_of(account_of(vault)));
+    BTCRelayCall::initialize(raw_init_block_header.into(), height)
+        .dispatch(origin_of(account_of(ALICE)));
+
+    height += 1;
 
     let address = Address::from(*dest_address.as_fixed_bytes());
     let value = amount as i64;
@@ -90,15 +97,34 @@ pub fn generate_transaction_and_mine(
         .add_transaction(transaction.clone())
         .mine(U256::from(2).pow(254.into()));
 
+    let raw_block_header = Formattable::format(&block.header);
+
     let tx_id = transaction.tx_id();
-    let tx_block_height = block.header.
+    let tx_block_height = height;
     let proof = block.merkle_proof(&vec![tx_id]);
     let bytes_proof = proof.format();
+    let raw_tx = Formattable::format_with(&transaction, true);
+
+    BTCRelayCall::store_block_header(raw_block_header.into())
+        .dispatch(origin_of(account_of(ALICE)));
 
     // FIXME: mine six new blocks to get over required confirmations
+    let mut prev_block_hash = block.header.hash();
+    for 0..confirmations {
+        let conf_block = BlockBuilder::new()
+            .with_previous_hash(prev_block_hash)
+            .with_version(2)
+            .with_coinbase(&address, 50, 3)
+            .with_timestamp(1588813835)
+            .mine(U256::from(2).pow(254.into()));
 
+        let raw_conf_block_header = Formattable::format(&conf_block.header);
+        btc_relay::Call<Runtime>::store_block_header(raw_conf_block_header)
+            .dispatch(origin_of(account_of(ALICE)));
+        prev_block_hash = conf_block.header.hash();
+    }
 
-    return (tx_id, 2, bytes_proof, raw_tx);
+    return (tx_id, height, bytes_proof, raw_tx);
 }
 
 
