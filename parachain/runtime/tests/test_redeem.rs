dbda85beb17c0dd4e42e372d77a818609904a40a
Dominik Harz <dominik@interlay.io>
2020-09-28 14:27:06 +0200

    progress parachain time on tests

--- parachain/runtime/tests/test_redeem.rs
+++ parachain/runtime/tests/test_redeem.rs
@@ -69,7 +69,7 @@ fn integration_test_redeem_polka_btc() {
         // assert that request happened and extract the id
         let redeem_id = assert_redeem_request_event();
 
-        SystemModule::set_block_number(5);
+        SystemModule::set_block_number(1 + CONFIRMATIONS);
 
         // send the btc from the vault to the user
         let (tx_id, tx_block_height, merkle_proof, raw_tx) =
