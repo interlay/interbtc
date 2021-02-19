7c2101aa89610c821fcf656f75062092090540a3
Gregory Hill <gregorydhill@outlook.com>
2021-02-19 16:30:22 +0000

    feat: adds account_id to RichBlockHeader to track submitter for fault attribution
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- crates/bitcoin/src/types.rs
+++ crates/bitcoin/src/types.rs
@@ -331,25 +331,36 @@ impl Transaction {
 
 /// Bitcoin Enriched Block Headers
 #[derive(Encode, Decode, Default, Clone, Copy, PartialEq, Eq, Debug)]
-pub struct RichBlockHeader {
+pub struct RichBlockHeader<AccountId> {
     pub block_hash: H256Le,
     pub block_header: BlockHeader,
     pub block_height: u32,
     pub chain_ref: u32,
+    // required for fault attribution
+    pub account_id: AccountId,
 }
 
-impl RichBlockHeader {
-    // Creates a RichBlockHeader given a RawBlockHeader, Blockchain identifier and block height
-    pub fn construct(
+impl<AccountId> RichBlockHeader<AccountId> {
+    /// Creates a new RichBlockHeader
+    ///
+    /// # Arguments
+    ///
+    /// * `raw_block_header` - 80 byte raw Bitcoin block header
+    /// * `chain_ref` - chain reference
+    /// * `block_height` - chain height
+    /// * `account_id` - submitter
+    pub fn new(
         raw_block_header: RawBlockHeader,
         chain_ref: u32,
         block_height: u32,
-    ) -> Result<RichBlockHeader, Error> {
+        account_id: AccountId,
+    ) -> Result<Self, Error> {
         Ok(RichBlockHeader {
             block_hash: raw_block_header.hash(),
             block_header: BlockHeader::from_le_bytes(raw_block_header.as_bytes())?,
             block_height,
             chain_ref,
+            account_id,
         })
     }
 }
