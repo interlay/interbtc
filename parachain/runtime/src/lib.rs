f4a6b877beb8ec928f6a63b626dcbb8f9a9ac7f5
Gregory Hill <gregorydhill@outlook.com>
2021-03-12 09:29:42 +0000

    chore: bump spec version for 0.5.5
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- parachain/runtime/src/lib.rs
+++ parachain/runtime/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("btc-parachain"),
     impl_name: create_runtime_str!("btc-parachain"),
     authoring_version: 1,
-    spec_version: 4,
+    spec_version: 5,
     impl_version: 1,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
