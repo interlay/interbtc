f2083a78947378806239d7c47e79a48f625e102a
Gregory Hill <gregorydhill@outlook.com>
2021-03-16 18:30:48 +0000

    chore: upgrade rococo-v1 dependencies
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- parachain/runtime/src/lib.rs
+++ parachain/runtime/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("btc-parachain"),
     impl_name: create_runtime_str!("btc-parachain"),
     authoring_version: 1,
-    spec_version: 6,
+    spec_version: 7,
     impl_version: 1,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
