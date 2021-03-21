797ff6cb1a915613f79cd0b41ca48bd4c3434183
Sander Bosma <sanderbosma@gmail.com>
2021-03-21 17:26:40 +0100

    feat!: include fee in RequestRefund event

--- parachain/runtime/tests/mock/issue_testing_utils.rs
+++ parachain/runtime/tests/mock/issue_testing_utils.rs
@@ -163,7 +163,7 @@ pub fn assert_refund_request_event() -> H256 {
     SystemModule::events()
         .iter()
         .find_map(|record| match record.event {
-            Event::refund(RefundEvent::RequestRefund(id, _, _, _, _, _)) => Some(id),
+            Event::refund(RefundEvent::RequestRefund(id, _, _, _, _, _, _)) => Some(id),
             _ => None,
         })
         .expect("request refund event not found")
