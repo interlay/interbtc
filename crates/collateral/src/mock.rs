6c6b2b09351a51a2df2d413e5687716d1f17021a
Gregory Hill <gregorydhill@outlook.com>
2021-04-06 09:31:02 +0100

    chore: upgrade rococo-v1 dependencies
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- crates/collateral/src/mock.rs
+++ crates/collateral/src/mock.rs
@@ -55,6 +55,7 @@ impl frame_system::Config for Test {
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
     type SS58Prefix = SS58Prefix;
+    type OnSetCode = ();
 }
 
 parameter_types! {
