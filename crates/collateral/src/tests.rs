c96bf7acc5dd26f207f2129122539669d22bed29
Gregory Hill <gregorydhill@outlook.com>
2021-04-27 21:06:59 +0100

    refactor: convert collateral pallet to FRAME v2
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- crates/collateral/src/tests.rs
+++ crates/collateral/src/tests.rs
@@ -1,7 +1,8 @@
 /// Tests for Collateral
 use crate::mock::*;
-use crate::RawEvent;
 use frame_support::{assert_err, assert_ok};
+
+type Event = crate::Event<Test>;
 
 /// Total supply
 #[test]
@@ -48,7 +49,7 @@ fn test_lock_collateral_succeeds() {
         let init_total = Collateral::get_total_collateral();
 
         assert_ok!(Collateral::lock_collateral(&sender, amount));
-        let lock_event = TestEvent::collateral(RawEvent::LockCollateral(ALICE, amount));
+        let lock_event = TestEvent::collateral(Event::LockCollateral(ALICE, amount));
 
         assert!(System::events().iter().any(|a| a.event == lock_event));
 
@@ -95,7 +96,7 @@ fn test_release_collateral_succeeds() {
         let init_total = Collateral::get_total_collateral();
 
         assert_ok!(Collateral::release_collateral(&sender, amount));
-        let release_event = TestEvent::collateral(RawEvent::ReleaseCollateral(ALICE, amount));
+        let release_event = TestEvent::collateral(Event::ReleaseCollateral(ALICE, amount));
 
         assert!(System::events().iter().any(|a| a.event == release_event));
 
@@ -142,7 +143,7 @@ fn test_release_collateral_partially_succeeds() {
         let init_total = Collateral::get_total_collateral();
 
         assert_ok!(Collateral::release_collateral(&sender, release_amount));
-        let release_event = TestEvent::collateral(RawEvent::ReleaseCollateral(ALICE, release_amount));
+        let release_event = TestEvent::collateral(Event::ReleaseCollateral(ALICE, release_amount));
 
         assert!(System::events().iter().any(|a| a.event == release_event));
 
@@ -169,7 +170,7 @@ fn test_slash_collateral_succeeds() {
         let init_total = Collateral::get_total_collateral();
 
         assert_ok!(Collateral::slash_collateral(sender, receiver, amount));
-        let slash_event = TestEvent::collateral(RawEvent::SlashCollateral(ALICE, BOB, amount));
+        let slash_event = TestEvent::collateral(Event::SlashCollateral(ALICE, BOB, amount));
 
         assert!(System::events().iter().any(|a| a.event == slash_event));
 
@@ -224,7 +225,7 @@ fn test_slash_collateral_partially_succeeds() {
         let init_total = Collateral::get_total_collateral();
 
         assert_ok!(Collateral::slash_collateral(sender, receiver, slash_amount));
-        let slash_event = TestEvent::collateral(RawEvent::SlashCollateral(ALICE, BOB, slash_amount));
+        let slash_event = TestEvent::collateral(Event::SlashCollateral(ALICE, BOB, slash_amount));
 
         assert!(System::events().iter().any(|a| a.event == slash_event));
 
