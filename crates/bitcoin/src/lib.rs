b89d996bc4b07df5f110e2e31495181f6268b0c0
Gregory Hill <gregorydhill@outlook.com>
2021-06-02 17:21:36 +0100

    chore: bitcoin and btc-relay lib doc comments
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- crates/bitcoin/src/lib.rs
+++ crates/bitcoin/src/lib.rs
@@ -1,3 +1,20 @@
+//! # Bitcoin Library
+//!
+//! This crate provides low-level parsing and verification functionality for Bitcoin specific data structures.
+//!
+//! Unless otherwise stated, the primary source of truth for code contained herein is the
+//! [Bitcoin Core repository](https://github.com/bitcoin/bitcoin), though implementation
+//! details may vary.
+//!
+//! ## Overview
+//!
+//! This crate provides functions for:
+//!
+//! - (De)serialization of block headers, transactions and merkle proofs.
+//! - Script (address) construction and parsing.
+//! - Merkle proof construction and verification.
+//! - Elliptic curve multiplication over Secp256k1.
+
 #![deny(warnings)]
 #![cfg_attr(test, feature(proc_macro_hygiene))]
 #![cfg_attr(not(feature = "std"), no_std)]
