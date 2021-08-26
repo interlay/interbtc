13918bc9e8dc2675aebf6878791838024a5ed384
Sander Bosma <sanderbosma@gmail.com>
2021-08-26 08:01:35 +0200

    fix: zero-cost base extrinsic weight, to be able to do runtime upgrade without fees

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -124,7 +124,7 @@ parameter_types! {
     pub RuntimeBlockWeights: BlockWeights = BlockWeights::builder()
         .base_block(BlockExecutionWeight::get())
         .for_class(DispatchClass::all(), |weights| {
-            weights.base_extrinsic = ExtrinsicBaseWeight::get();
+            weights.base_extrinsic = 0;  // TODO: this is 0 so that we can do runtime upgrade without fees. Restore value afterwards!
         })
         .for_class(DispatchClass::Normal, |weights| {
             weights.max_total = Some(NORMAL_DISPATCH_RATIO * MAXIMUM_BLOCK_WEIGHT);
@@ -211,7 +211,7 @@ impl pallet_timestamp::Config for Runtime {
 }
 
 parameter_types! {
-    pub const TransactionByteFee: Balance = 1;
+    pub const TransactionByteFee: Balance = 0;  // TODO: this is 0 so that we can do runtime upgrade without fees. Restore value afterwards!
 }
 
 impl pallet_transaction_payment::Config for Runtime {
