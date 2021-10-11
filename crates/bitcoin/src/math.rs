366d17afa758a95089ac4fe9eaa68d461205ff58
Gregory Hill <gregorydhill@outlook.com>
2021-10-11 10:50:16 +0100

    test: GetCompact and SetCompact
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- crates/bitcoin/src/math.rs
+++ crates/bitcoin/src/math.rs
@@ -43,7 +43,15 @@ impl SetCompact for U256 {
         Self: Sized,
     {
         let size = compact >> 24;
-        let word = compact & 0x007fffff;
+        let mut word = compact & 0x007fffff;
+
+        let value = if size <= 3 {
+            word >>= 8 * (3 - size);
+            U256::from(word)
+        } else {
+            let word = U256::from(word);
+            word << 8 * (size - 3)
+        };
 
         if word != 0 && (compact & 0x00800000) != 0 {
             // negative
@@ -52,12 +60,44 @@ impl SetCompact for U256 {
             // overflow
             None
         } else {
-            let word = U256::from(word);
-            Some(if size <= 3 {
-                word >> 8 * (3 - size)
-            } else {
-                word << 8 * (size - 3)
-            })
+            Some(value)
+        }
+    }
+}
+
+// https://github.com/bitcoin/bitcoin/blob/7fcf53f7b4524572d1d0c9a5fdc388e87eb02416/src/test/arith_uint256_tests.cpp
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bignum_set_compact() {
+        // make sure that we don't generate compacts with the 0x00800000 bit set
+        assert_eq!(U256::from(0x80).get_compact(), Some(0x02008000));
+
+        for (input, output) in [
+            (0, Some(0)),
+            (0x00123456, Some(0)),
+            (0x01003456, Some(0)),
+            (0x02000056, Some(0)),
+            (0x03000000, Some(0)),
+            (0x04000000, Some(0)),
+            (0x00923456, Some(0)),
+            (0x01803456, Some(0)),
+            (0x02800056, Some(0)),
+            (0x03800000, Some(0)),
+            (0x04800000, Some(0)),
+            (0x01123456, Some(0x01120000)),
+            (0x01fedcba, None), // negative
+            (0x02123456, Some(0x02123400)),
+            (0x03123456, Some(0x03123456)),
+            (0x04123456, Some(0x04123456)),
+            (0x04923456, None), // negative
+            (0x05009234, Some(0x05009234)),
+            (0x20123456, Some(0x20123456)),
+            (0xff123456, None), // overflow
+        ] {
+            assert_eq!(U256::set_compact(input).and_then(|num| num.get_compact()), output)
         }
     }
 }
