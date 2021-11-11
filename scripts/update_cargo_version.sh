02841ac1c36651763d8244830080c65912acf98d
ns212 <nikolai@interlay.io>
2021-11-11 08:19:51 +0000

    Chore: update software version in Cargo.toml

---
+++ scripts/update_cargo_version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+TAG=$(git describe --exact-match --tags HEAD  2> /dev/null)
+FOUND_TAG=$?
+SED_PATTERN="s/^\\(version *= *\\).*/\\1\"$TAG\"/"
+
+if [ $FOUND_TAG -ne 0 ]; then
+    TAG=-$(git rev-parse --short HEAD)
+    SED_PATTERN="s/^\(version *= *\)\"\(.*\)\"/\1\"\2$TAG\"/"
+fi
+
+sed -i "$SED_PATTERN" $1
