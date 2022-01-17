2f9961acab5fd9be7e58709177b9dea8526ef2ed
Gregory Hill <gregorydhill@outlook.com>
2022-01-17 14:35:35 +0000

    chore: upgrade to polkadot-v0.9.15
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- parachain/runtime/kintsugi/src/lib.rs
+++ parachain/runtime/kintsugi/src/lib.rs
@@ -65,7 +65,7 @@ pub use primitives::{
 
 // XCM imports
 use cumulus_primitives_core::ParaId;
-use orml_xcm_support::{IsNativeConcrete, MultiCurrencyAdapter, MultiNativeAsset};
+use orml_xcm_support::{DepositToAlternative, IsNativeConcrete, MultiCurrencyAdapter, MultiNativeAsset};
 use pallet_transaction_payment::{Multiplier, TargetedFeeAdjustment};
 use pallet_xcm::XcmPassthrough;
 use polkadot_parachain::primitives::Sibling;
@@ -740,6 +740,7 @@ pub type LocalAssetTransactor = MultiCurrencyAdapter<
     LocationToAccountId,
     CurrencyId,
     CurrencyIdConvert,
+    DepositToAlternative<TreasuryAccount, Tokens, CurrencyId, AccountId, Balance>,
 >;
 
 pub use currency_id_convert::CurrencyIdConvert;
@@ -1284,7 +1285,7 @@ pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signatu
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Call, SignedExtra>;
 /// Executive: handles dispatch to the various modules.
 pub type Executive =
-    frame_executive::Executive<Runtime, Block, frame_system::ChainContext<Runtime>, Runtime, AllPallets>;
+    frame_executive::Executive<Runtime, Block, frame_system::ChainContext<Runtime>, Runtime, AllPalletsWithSystem>;
 
 #[cfg(not(feature = "disable-runtime-api"))]
 impl_runtime_apis! {
