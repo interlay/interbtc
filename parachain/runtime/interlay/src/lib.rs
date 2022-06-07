c3d3bf4df9ffb663e986cfede5f72464545e3726
Gregory Hill <gregorydhill@outlook.com>
2022-06-07 13:31:28 +0100

    chore: bump spec versions for 1.14.0
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- parachain/runtime/interlay/src/lib.rs
+++ parachain/runtime/interlay/src/lib.rs
@@ -88,7 +88,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("interlay-parachain"),
     impl_name: create_runtime_str!("interlay-parachain"),
     authoring_version: 1,
-    spec_version: 3, // 1.12.0
+    spec_version: 3, // 1.14.0
     impl_version: 1,
     transaction_version: 2, // added preimage
     apis: RUNTIME_API_VERSIONS,
