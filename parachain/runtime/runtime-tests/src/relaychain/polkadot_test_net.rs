1bdd11a208699fa53372ada750590bd3b485443f
Sander Bosma <sanderbosma@gmail.com>
2022-06-17 16:45:09 +0200

    test: interlay xcm tests

--- parachain/runtime/runtime-tests/src/relaychain/polkadot_test_net.rs
+++ parachain/runtime/runtime-tests/src/relaychain/polkadot_test_net.rs
@@ -1,51 +1,51 @@
 use frame_support::traits::GenesisBuild;
-pub use kintsugi_runtime_parachain::{xcm_config::*, *};
+pub use interlay_runtime_parachain::{xcm_config::*, *};
 use polkadot_primitives::v2::{BlockNumber, MAX_CODE_SIZE, MAX_POV_SIZE};
 use polkadot_runtime_parachains::configuration::HostConfiguration;
 pub use primitives::{
     CurrencyId::Token,
-    TokenSymbol::{KINT, KSM},
+    TokenSymbol::{DOT, INTR},
 };
 use sp_runtime::traits::AccountIdConversion;
 use xcm_emulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain};
 
-pub const KINTSUGI_PARA_ID: u32 = 2092;
+pub const INTERLAY_PARA_ID: u32 = 2032;
 pub const SIBLING_PARA_ID: u32 = 2001;
 
 decl_test_relay_chain! {
-    pub struct KusamaNet {
-        Runtime = kusama_runtime::Runtime,
-        XcmConfig = kusama_runtime::xcm_config::XcmConfig,
-        new_ext = kusama_ext(),
+    pub struct PolkadotNet {
+        Runtime = polkadot_runtime::Runtime,
+        XcmConfig = polkadot_runtime::xcm_config::XcmConfig,
+        new_ext = polkadot_ext(),
     }
 }
 
 decl_test_parachain! {
-    pub struct Kintsugi {
+    pub struct Interlay {
         Runtime = Runtime,
         Origin = Origin,
-        XcmpMessageHandler = kintsugi_runtime_parachain::XcmpQueue,
-        DmpMessageHandler = kintsugi_runtime_parachain::DmpQueue,
-        new_ext = para_ext(KINTSUGI_PARA_ID),
+        XcmpMessageHandler = interlay_runtime_parachain::XcmpQueue,
+        DmpMessageHandler = interlay_runtime_parachain::DmpQueue,
+        new_ext = para_ext(INTERLAY_PARA_ID),
     }
 }
 
 decl_test_parachain! {
     pub struct Sibling {
-        Runtime = testnet_kintsugi_runtime_parachain::Runtime,
-        Origin = testnet_kintsugi_runtime_parachain::Origin,
-        XcmpMessageHandler = testnet_kintsugi_runtime_parachain::XcmpQueue,
-        DmpMessageHandler = testnet_kintsugi_runtime_parachain::DmpQueue,
+        Runtime = testnet_interlay_runtime_parachain::Runtime,
+        Origin = testnet_interlay_runtime_parachain::Origin,
+        XcmpMessageHandler = testnet_interlay_runtime_parachain::XcmpQueue,
+        DmpMessageHandler = testnet_interlay_runtime_parachain::DmpQueue,
         new_ext = para_ext(SIBLING_PARA_ID),
     }
 }
 
-// note: can't use SIBLING_PARA_ID and KINTSUGI_PARA_ID in this macro - we are forced to use raw numbers
+// note: can't use SIBLING_PARA_ID and INTERLAY_PARA_ID in this macro - we are forced to use raw numbers
 decl_test_network! {
     pub struct TestNet {
-        relay_chain = KusamaNet,
+        relay_chain = PolkadotNet,
         parachains = vec![
-            (2092, Kintsugi),
+            (2032, Interlay),
             (2001, Sibling),
         ],
     }
@@ -53,55 +53,62 @@ decl_test_network! {
 
 fn default_parachains_host_configuration() -> HostConfiguration<BlockNumber> {
     HostConfiguration {
-        minimum_validation_upgrade_delay: 5,
-        validation_upgrade_cooldown: 5u32,
-        validation_upgrade_delay: 5,
-        code_retention_period: 1200,
         max_code_size: MAX_CODE_SIZE,
         max_pov_size: MAX_POV_SIZE,
-        max_head_data_size: 32 * 1024,
-        group_rotation_frequency: 20,
-        chain_availability_period: 4,
-        thread_availability_period: 4,
-        max_upward_queue_count: 8,
-        max_upward_queue_size: 1024 * 1024,
-        max_downward_message_size: 1024,
-        ump_service_total_weight: 4 * 1_000_000_000,
-        max_upward_message_size: 50 * 1024,
-        max_upward_message_num_per_candidate: 5,
-        hrmp_sender_deposit: 5_000_000_000_000,
-        hrmp_recipient_deposit: 5_000_000_000_000,
-        hrmp_channel_max_capacity: 1000,
-        hrmp_channel_max_total_size: 8 * 1024,
-        hrmp_max_parachain_inbound_channels: 4,
-        hrmp_max_parathread_inbound_channels: 4,
-        hrmp_channel_max_message_size: 102400,
-        hrmp_max_parachain_outbound_channels: 4,
-        hrmp_max_parathread_outbound_channels: 4,
-        hrmp_max_message_num_per_candidate: 5,
+        max_head_data_size: 20_480,
+        max_upward_queue_count: 10,
+        max_upward_queue_size: 51_200,
+        max_upward_message_size: 51_200,
+        max_upward_message_num_per_candidate: 10,
+        hrmp_max_message_num_per_candidate: 10,
+        validation_upgrade_cooldown: 14_400,
+        validation_upgrade_delay: 600,
+        max_downward_message_size: 51_200,
+        ump_service_total_weight: 100_000_000_000,
+        hrmp_max_parachain_outbound_channels: 10,
+        hrmp_max_parathread_outbound_channels: 0,
+        hrmp_sender_deposit: 100_000_000_000,
+        hrmp_recipient_deposit: 100_000_000_000,
+        hrmp_channel_max_capacity: 1_000,
+        hrmp_channel_max_total_size: 102_400,
+        hrmp_max_parachain_inbound_channels: 10,
+        hrmp_max_parathread_inbound_channels: 0,
+        hrmp_channel_max_message_size: 102_400,
+        code_retention_period: 14_400,
+        parathread_cores: 0,
+        parathread_retries: 0,
+        group_rotation_frequency: 10,
+        chain_availability_period: 10,
+        thread_availability_period: 10,
+        scheduling_lookahead: 1,
+        max_validators_per_core: Some(5),
+        max_validators: Some(200),
         dispute_period: 6,
+        dispute_post_conclusion_acceptance_period: 600,
+        dispute_max_spam_slots: 2,
+        dispute_conclusion_by_time_out_period: 600,
         no_show_slots: 2,
-        n_delay_tranches: 25,
-        needed_approvals: 2,
-        relay_vrf_modulo_samples: 2,
+        n_delay_tranches: 89,
         zeroth_delay_tranche_width: 0,
-        ..Default::default()
+        needed_approvals: 30,
+        relay_vrf_modulo_samples: 40,
+        ump_max_individual_weight: 20_000_000_000,
+        pvf_checking_enabled: false,
+        pvf_voting_ttl: 2,
+        minimum_validation_upgrade_delay: 20,
     }
 }
 
-pub fn kusama_ext() -> sp_io::TestExternalities {
-    use kusama_runtime::{Runtime, System};
+pub fn polkadot_ext() -> sp_io::TestExternalities {
     use polkadot_parachain::primitives::{HeadData, ValidationCode};
+    use polkadot_runtime::{Runtime, System};
 
     let mut t = frame_system::GenesisConfig::default()
         .build_storage::<Runtime>()
         .unwrap();
 
     pallet_balances::GenesisConfig::<Runtime> {
-        balances: vec![
-            (AccountId::from(ALICE), 2002 * KSM.one()),
-            // (ParaId::from(KINTSUGI_PARA_ID).into_account_truncating(), 2 * KSM.one()),
-        ],
+        balances: vec![(AccountId::from(ALICE), 100_000_000 * DOT.one())],
     }
     .assimilate_storage(&mut t)
     .unwrap();
@@ -115,7 +122,7 @@ pub fn kusama_ext() -> sp_io::TestExternalities {
     <polkadot_runtime_parachains::paras::GenesisConfig as GenesisBuild<Runtime>>::assimilate_storage(
         &polkadot_runtime_parachains::paras::GenesisConfig {
             paras: vec![
-                (KINTSUGI_PARA_ID.into(), fake_para.clone()),
+                (INTERLAY_PARA_ID.into(), fake_para.clone()),
                 (SIBLING_PARA_ID.into(), fake_para.clone()),
             ],
         },
@@ -144,10 +151,7 @@ pub fn kusama_ext() -> sp_io::TestExternalities {
 
 pub fn para_ext(parachain_id: u32) -> sp_io::TestExternalities {
     ExtBuilder::default()
-        .balances(vec![
-            (AccountId::from(ALICE), Token(KSM), 10 * KSM.one()),
-            // (kintsugi_runtime_parachain::TreasuryAccount::get(), Token(KSM), KSM.one()),
-        ])
+        .balances(vec![(AccountId::from(ALICE), Token(DOT), 10 * DOT.one())])
         .parachain_id(parachain_id)
         .build()
 }
@@ -224,10 +228,10 @@ impl ExtBuilder {
     }
 }
 
-pub(crate) fn kintsugi_sovereign_account_on_kusama() -> AccountId {
-    polkadot_parachain::primitives::Id::from(KINTSUGI_PARA_ID).into_account_truncating()
+pub(crate) fn interlay_sovereign_account_on_polkadot() -> AccountId {
+    polkadot_parachain::primitives::Id::from(INTERLAY_PARA_ID).into_account_truncating()
 }
 
-pub(crate) fn sibling_sovereign_account_on_kusama() -> AccountId {
+pub(crate) fn sibling_sovereign_account_on_polkadot() -> AccountId {
     polkadot_parachain::primitives::Id::from(SIBLING_PARA_ID).into_account_truncating()
 }
