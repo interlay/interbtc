753296cbb5b6aba417fd11e06a81180fc485b23e
Dominik Harz <dominik@interlay.io>
2022-07-05 12:50:44 +0200

    hotfix: remove theft reporting

--- crates/relay/src/ext.rs
+++ crates/relay/src/ext.rs
@@ -4,8 +4,7 @@ use mocktopus::macros::mockable;
 #[cfg_attr(test, mockable)]
 pub(crate) mod vault_registry {
     use crate::DefaultVaultId;
-    use ::vault_registry::VaultStatus;
-    use frame_support::dispatch::{DispatchError, DispatchResult};
+    use frame_support::dispatch::DispatchError;
     use vault_registry::types::DefaultVault;
 
     pub fn get_active_vault_from_id<T: crate::Config>(
@@ -13,23 +12,11 @@ pub(crate) mod vault_registry {
     ) -> Result<DefaultVault<T>, DispatchError> {
         <vault_registry::Pallet<T>>::get_active_vault_from_id(vault_id)
     }
-
-    pub fn liquidate_theft_vault<T: crate::Config>(
-        vault_id: &DefaultVaultId<T>,
-        reporter_id: T::AccountId,
-    ) -> DispatchResult {
-        let _ = <vault_registry::Pallet<T>>::liquidate_vault_with_status(
-            vault_id,
-            VaultStatus::CommittedTheft,
-            Some(reporter_id),
-        )?;
-        Ok(())
-    }
 }
 
 #[cfg_attr(test, mockable)]
 pub(crate) mod btc_relay {
-    use bitcoin::types::{BlockHeader, H256Le, MerkleProof, RawBlockHeader};
+    use bitcoin::types::{BlockHeader, RawBlockHeader};
     use frame_support::dispatch::DispatchResult;
     use sp_runtime::DispatchError;
 
@@ -45,21 +32,10 @@ pub(crate) mod btc_relay {
         <btc_relay::Pallet<T>>::store_block_header(relayer, block_header)
     }
 
-    pub(crate) fn verify_transaction_inclusion<T: crate::Config>(
-        tx_id: H256Le,
-        merkle_proof: MerkleProof,
-    ) -> DispatchResult {
-        <btc_relay::Pallet<T>>::_verify_transaction_inclusion(tx_id, merkle_proof, None)
-    }
-
     pub fn parse_raw_block_header<T: btc_relay::Config>(
         raw_block_header: &RawBlockHeader,
     ) -> Result<BlockHeader, DispatchError> {
         <btc_relay::Pallet<T>>::parse_raw_block_header(raw_block_header)
-    }
-
-    pub fn parse_merkle_proof<T: btc_relay::Config>(raw_merkle_proof: &[u8]) -> Result<MerkleProof, DispatchError> {
-        <btc_relay::Pallet<T>>::parse_merkle_proof(raw_merkle_proof)
     }
 }
 
