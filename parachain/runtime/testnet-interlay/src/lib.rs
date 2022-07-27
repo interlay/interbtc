2fd776ec739d7b19c7069127dbc653244b65650c
Gregory Hill <gregorydhill@outlook.com>
2022-07-27 13:37:05 +0100

    refactor!: testnet ss58s should be same as the primary runtimes
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- parachain/runtime/testnet-interlay/src/lib.rs
+++ parachain/runtime/testnet-interlay/src/lib.rs
@@ -152,7 +152,7 @@ parameter_types! {
         })
         .avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
         .build_or_panic();
-    pub const SS58Prefix: u16 = 42;
+    pub const SS58Prefix: u16 = 2032;
 }
 
 pub struct BaseCallFilter;
