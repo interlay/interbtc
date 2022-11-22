f2400f7e5a5c0d1176ec01d5ca5e4780a3cf16f9
Daniel Savu <savudani04@yahoo.ro>
2022-11-22 16:15:54 +0100

    chore: bump spec_version to 1.20.0

--- parachain/runtime/testnet-kintsugi/src/lib.rs
+++ parachain/runtime/testnet-kintsugi/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("testnet-kintsugi"),
     impl_name: create_runtime_str!("testnet-kintsugi"),
     authoring_version: 1,
-    spec_version: 1019000,
+    spec_version: 1020000,
     impl_version: 1,
     transaction_version: 1, // added preimage
     apis: RUNTIME_API_VERSIONS,
