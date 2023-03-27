1c5cf6b0ae3d055385564b02978350238007322b
Sander Bosma <sanderbosma@gmail.com>
2023-03-27 21:36:12 +0200

    chore: fix escrow benchmark

--- crates/escrow/src/benchmarking.rs
+++ crates/escrow/src/benchmarking.rs
@@ -1,16 +1,16 @@
 use super::*;
-use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite};
+use frame_benchmarking::v2::{account, benchmarks, impl_benchmark_test_suite};
 use frame_support::assert_ok;
 use frame_system::RawOrigin;
-
+use sp_std::vec;
 // Pallets
 use crate::Pallet as Escrow;
 use frame_system::Pallet as System;
 
-fn create_default_lock<T: Config>(origin: T::AccountId) {
+pub fn create_default_lock<T: Config>(origin: T::AccountId) {
     let start_height = System::<T>::block_number();
     let end_height = start_height + T::MaxPeriod::get();
-    let amount: u32 = 1000;
+    let amount = T::BlockNumberToBalance::convert(T::MaxPeriod::get());
     T::Currency::make_free_balance_be(&origin, amount.into());
     assert_ok!(Escrow::<T>::create_lock(
         RawOrigin::Signed(origin).into(),
@@ -19,37 +19,55 @@ fn create_default_lock<T: Config>(origin: T::AccountId) {
     ));
 }
 
-benchmarks! {
-    create_lock {
+#[benchmarks]
+pub mod benchmarks {
+    use super::*;
+
+    #[benchmark]
+    pub fn create_lock() {
         let origin: T::AccountId = account("Origin", 0, 0);
         let start_height = System::<T>::block_number();
         let end_height = start_height + T::MaxPeriod::get();
-        let amount: u32 = 1000;
+        let amount = T::BlockNumberToBalance::convert(T::MaxPeriod::get());
         T::Currency::make_free_balance_be(&origin, amount.into());
-    }: _(RawOrigin::Signed(origin), amount.into(), end_height)
 
-    increase_amount {
+        #[extrinsic_call]
+        create_lock(RawOrigin::Signed(origin), amount.into(), end_height);
+    }
+
+    #[benchmark]
+    pub fn increase_amount() {
         let origin: T::AccountId = account("Origin", 0, 0);
-        let amount: u32 = 1000;
+        let amount = T::BlockNumberToBalance::convert(T::MaxPeriod::get());
         create_default_lock::<T>(origin.clone());
         let free_balance = T::Currency::free_balance(&origin);
         T::Currency::make_free_balance_be(&origin, free_balance + amount.into());
-    }: _(RawOrigin::Signed(origin), amount.into())
 
-    increase_unlock_height {
+        #[extrinsic_call]
+        increase_amount(RawOrigin::Signed(origin), amount.into());
+    }
+
+    #[benchmark]
+    pub fn increase_unlock_height() {
         let origin: T::AccountId = account("Origin", 0, 0);
         create_default_lock::<T>(origin.clone());
         let end_height = System::<T>::block_number() + T::MaxPeriod::get() - T::Span::get();
         System::<T>::set_block_number(end_height);
-    }: _(RawOrigin::Signed(origin), end_height + T::MaxPeriod::get())
 
-    withdraw {
+        #[extrinsic_call]
+        increase_unlock_height(RawOrigin::Signed(origin), end_height + T::MaxPeriod::get());
+    }
+
+    #[benchmark]
+    pub fn withdraw() {
         let origin: T::AccountId = account("Origin", 0, 0);
         create_default_lock::<T>(origin.clone());
         let end_height = System::<T>::block_number() + T::MaxPeriod::get() + T::Span::get();
         System::<T>::set_block_number(end_height);
-    }: _(RawOrigin::Signed(origin))
 
+        #[extrinsic_call]
+        withdraw(RawOrigin::Signed(origin));
+    }
+
+    impl_benchmark_test_suite! {Escrow, crate::mock::ExtBuilder::build(), crate::mock::Test}
 }
-
-impl_benchmark_test_suite!(Escrow, crate::mock::ExtBuilder::build(), crate::mock::Test);
