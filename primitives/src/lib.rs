15558c7e718919bf667f018f59515b426cc054a4
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2023-03-30 16:48:31 +0200

    fix(dex-stable): benchmark runtime errors

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -562,6 +562,11 @@ impl dex_general::AssetInfo for CurrencyId {
 #[cfg(feature = "runtime-benchmarks")]
 impl From<u32> for CurrencyId {
     fn from(value: u32) -> Self {
-        CurrencyId::ForeignAsset(value)
+        if value < 1000 {
+            // Inner value must fit inside `u8`
+            CurrencyId::ForeignAsset((value % 256).try_into().unwrap())
+        } else {
+            CurrencyId::StableLpToken((value % 256).try_into().unwrap())
+        }
     }
 }
