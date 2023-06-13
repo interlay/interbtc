d868149a09ccf35fa968e351ea1ec9c6478d2b5f
Gregory Hill <gregorydhill@outlook.com>
2023-06-13 17:13:21 +0000

    refactor: move loans tests to parachain
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- parachain/runtime/runtime-tests/src/parachain.rs
+++ parachain/runtime/runtime-tests/src/parachain.rs
@@ -18,3 +18,6 @@ mod governance;
 
 #[cfg(any(feature = "with-interlay-runtime", feature = "with-kintsugi-runtime",))]
 mod issue;
+
+#[cfg(any(feature = "with-interlay-runtime", feature = "with-kintsugi-runtime",))]
+mod loans;
