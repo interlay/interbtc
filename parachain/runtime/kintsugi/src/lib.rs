7af6b84992aa70da72090eadb8f5b6f92b404c39
Sander Bosma <sanderbosma@gmail.com>
2023-06-29 12:43:50 +0200

    chore: add contracts pallet to kintsugi

--- parachain/runtime/kintsugi/src/lib.rs
+++ parachain/runtime/kintsugi/src/lib.rs
@@ -16,18 +16,22 @@ use frame_support::{
     dispatch::{DispatchError, DispatchResult},
     traits::{
         ConstU32, Contains, Currency as PalletCurrency, EitherOfDiverse, EnsureOrigin, EnsureOriginWithArg,
-        EqualPrivilegeOnly, ExistenceRequirement, Imbalance, InstanceFilter, OnUnbalanced,
+        EqualPrivilegeOnly, ExistenceRequirement, Imbalance, InstanceFilter, OnUnbalanced, tokens::{Provenance, Fortitude}, ReservableCurrency, WithdrawReasons, SignedImbalance,
     },
     weights::ConstantMultiplier,
     PalletId,
 };
+use frame_support::traits::tokens::DepositConsequence;
+use frame_support::traits::tokens::WithdrawConsequence;
+use frame_support::traits::tokens::Preservation;
+use frame_support::traits::fungible::Inspect;
 use frame_system::{
     limits::{BlockLength, BlockWeights},
     EnsureRoot, RawOrigin,
 };
 use loans::{OnSlashHook, PostDeposit, PostTransfer, PreDeposit, PreTransfer};
 use orml_asset_registry::SequentialId;
-use orml_traits::{currency::MutationHooks, parameter_type_with_key};
+use orml_traits::{currency::MutationHooks, parameter_type_with_key, BalanceStatus};
 use pallet_transaction_payment::{Multiplier, TargetedFeeAdjustment};
 use sp_api::impl_runtime_apis;
 use sp_core::{OpaqueMetadata, H256};
@@ -781,7 +785,7 @@ impl supply::Config for Runtime {
 pub struct TotalWrapped;
 impl Get<Balance> for TotalWrapped {
     fn get() -> Balance {
-        orml_tokens::CurrencyAdapter::<Runtime, GetWrappedCurrencyId>::total_issuance()
+        <orml_tokens::CurrencyAdapter::<Runtime, GetWrappedCurrencyId> as Inspect<AccountId>>::total_issuance()
     }
 }
 
@@ -1183,6 +1187,231 @@ impl loans::Config for Runtime {
     type OnExchangeRateChange = vault_registry::PoolManager<Runtime>;
 }
 
+use frame_support::traits::Randomness;
+pub struct NoRandomness;
+impl Randomness<Hash, BlockNumber> for NoRandomness {
+    fn random(_subject: &[u8]) -> (Hash, BlockNumber) {
+        // this is deprecated so don't bother to implement
+        unimplemented!()
+    }
+}
+
+pub struct DummyWeightPrice;
+impl Convert<Weight, Balance> for DummyWeightPrice {
+    fn convert(_a: Weight) -> Balance {
+        // informational only, leaving blank for now
+        Default::default()
+    }
+}
+
+// contracts
+parameter_types! {
+    pub const DeletionQueueDepth: u32 = 10;
+    pub const DeletionWeightLimit: Weight = Weight::from_ref_time(100000000 as u64);
+    pub const DepositPerByte: Balance = 1;
+    pub const DepositPerItem: Balance = 1;
+    pub const MaxCodeLen: u32 = 100000000;
+    pub const MaxStorageKeyLen: u32 = 100000000;
+    pub const UnsafeUnstableInterface: bool = false;
+    pub const MaxDebugBufferLen: u32 = 100000000;
+    pub const DefaultDepositLimit: Balance = 1_000_000_000_000;
+}
+
+pub struct NativeCurrencyWithEd;
+
+impl Inspect<AccountId> for NativeCurrencyWithEd {
+    type Balance = Balance;
+
+    fn total_issuance() -> Self::Balance {
+        <NativeCurrency as Inspect<AccountId>>::total_issuance()
+    }
+    fn active_issuance() -> Self::Balance {
+        <NativeCurrency as Inspect<AccountId>>::active_issuance()
+    }
+    fn minimum_balance() -> Self::Balance {
+        1
+    }
+
+    fn total_balance(who: &AccountId) -> Self::Balance {
+        <NativeCurrency as Inspect<AccountId>>::total_balance(who)
+    }
+
+    fn balance(who: &AccountId) -> Self::Balance {
+        <NativeCurrency as Inspect<AccountId>>::balance(who)
+    }
+
+    fn reducible_balance(who: &AccountId, preservation: Preservation, force: Fortitude) -> Self::Balance {
+        <NativeCurrency as Inspect<AccountId>>::reducible_balance(who, preservation, force)
+    }
+
+    fn can_deposit(who: &AccountId, amount: Self::Balance, provenance: Provenance) -> DepositConsequence {
+        <NativeCurrency as Inspect<AccountId>>::can_deposit(who, amount, provenance)
+    }
+
+    fn can_withdraw(who: &AccountId, amount: Self::Balance) -> WithdrawConsequence<Self::Balance> {
+        <NativeCurrency as Inspect<AccountId>>::can_withdraw(who, amount)
+    }
+}
+
+impl  ReservableCurrency<AccountId> for NativeCurrencyWithEd {
+	fn can_reserve(who: &AccountId, value: Self::Balance) -> bool {
+        NativeCurrency::can_reserve(who, value)
+    }
+	fn slash_reserved(
+		who: &AccountId,
+		value: Self::Balance,
+	) -> (Self::NegativeImbalance, Self::Balance) {
+        NativeCurrency::slash_reserved(who, value)
+    }
+	fn reserved_balance(who: &AccountId) -> Self::Balance{
+        NativeCurrency::reserved_balance(who)
+    }
+	fn reserve(who: &AccountId, value: Self::Balance) -> DispatchResult{
+        NativeCurrency::reserve(who, value)
+    }
+	fn unreserve(who: &AccountId, value: Self::Balance) -> Self::Balance{
+        NativeCurrency::unreserve(who, value)
+    }
+	fn repatriate_reserved(
+		slashed: &AccountId,
+		beneficiary: &AccountId,
+		value: Self::Balance,
+		status: BalanceStatus,
+	) -> Result<Self::Balance, DispatchError> {
+        NativeCurrency::repatriate_reserved(slashed, beneficiary, value, status)
+    }
+}
+
+impl frame_support::traits::Currency<AccountId> for NativeCurrencyWithEd {
+	type Balance = <NativeCurrency as frame_support::traits::Currency<AccountId>>::Balance;
+	type NegativeImbalance = <NativeCurrency as frame_support::traits::Currency<AccountId>>::NegativeImbalance;
+	type PositiveImbalance = <NativeCurrency as frame_support::traits::Currency<AccountId>>::PositiveImbalance;
+    
+	fn total_balance(who: &AccountId) -> Self::Balance {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::total_balance(who)
+    }
+	fn can_slash(who: &AccountId, value: Self::Balance) -> bool {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::can_slash(who, value)
+    }
+	fn total_issuance() -> Self::Balance {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::total_issuance()
+    }
+	fn active_issuance() -> Self::Balance {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::active_issuance()
+    }
+	fn deactivate(x: Self::Balance) {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::deactivate(x)
+    }
+	fn reactivate(x: Self::Balance) {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::reactivate(x)
+    }
+	fn minimum_balance() -> Self::Balance {
+        1
+    }
+	fn burn(amount: Self::Balance) -> Self::PositiveImbalance{
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::burn(amount)
+    }
+	fn issue(amount: Self::Balance) -> Self::NegativeImbalance {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::issue(amount)
+    }
+	fn pair(amount: Self::Balance) -> (Self::PositiveImbalance, Self::NegativeImbalance) {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::pair(amount)
+    }
+	fn free_balance(who: &AccountId) -> Self::Balance{
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::free_balance(who)
+    }
+	fn ensure_can_withdraw(
+		who: &AccountId,
+		_amount: Self::Balance,
+		reasons: WithdrawReasons,
+		new_balance: Self::Balance,
+	) -> DispatchResult{
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::ensure_can_withdraw(who, _amount, reasons, new_balance)
+    }
+	fn transfer(
+		source: &AccountId,
+		dest: &AccountId,
+		value: Self::Balance,
+		existence_requirement: ExistenceRequirement,
+	) -> DispatchResult {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::transfer(source, dest, value, existence_requirement)
+    }
+	fn slash(who: &AccountId, value: Self::Balance) -> (Self::NegativeImbalance, Self::Balance){
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::slash(who, value)
+    }
+	fn deposit_into_existing(
+		who: &AccountId,
+		value: Self::Balance,
+	) -> Result<Self::PositiveImbalance, DispatchError>{
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::deposit_into_existing(who, value)
+    }
+	fn resolve_into_existing(
+		who: &AccountId,
+		value: Self::NegativeImbalance,
+	) -> Result<(), Self::NegativeImbalance> {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::resolve_into_existing(who, value)
+    }
+	fn deposit_creating(who: &AccountId, value: Self::Balance) -> Self::PositiveImbalance{
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::deposit_creating(who, value)
+    }
+	fn resolve_creating(who: &AccountId, value: Self::NegativeImbalance){
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::resolve_creating(who, value)
+    }
+	fn withdraw(
+		who: &AccountId,
+		value: Self::Balance,
+		reasons: WithdrawReasons,
+		liveness: ExistenceRequirement,
+	) -> Result<Self::NegativeImbalance, DispatchError>{
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::withdraw(who, value, reasons, liveness)
+    }
+	fn settle(
+		who: &AccountId,
+		value: Self::PositiveImbalance,
+		reasons: WithdrawReasons,
+		liveness: ExistenceRequirement,
+	) -> Result<(), Self::PositiveImbalance> {
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::settle(who, value, reasons, liveness)
+    }
+	fn make_free_balance_be(
+		who: &AccountId,
+		balance: Self::Balance,
+	) -> SignedImbalance<Self::Balance, Self::PositiveImbalance>{
+        <NativeCurrency as frame_support::traits::Currency<AccountId>>::make_free_balance_be(who, balance)
+    }
+}
+pub struct DefaultSchedule;
+
+impl Get<pallet_contracts::Schedule<Runtime>> for DefaultSchedule {
+    fn get() -> pallet_contracts::Schedule<Runtime> {
+        Default::default()
+    }
+}
+
+use pallet_contracts::chain_extension::{ChainExtension, Ext};
+
+impl pallet_contracts::Config for Runtime {
+    type Time = Timestamp;
+    type Randomness = NoRandomness;
+    type Currency = NativeCurrencyWithEd;
+    type RuntimeEvent = RuntimeEvent;
+    type RuntimeCall = RuntimeCall;
+    type CallFilter = BaseCallFilter;
+    type WeightPrice = DummyWeightPrice;
+    type WeightInfo = ();
+    type ChainExtension = ();
+    type Schedule = DefaultSchedule;
+    type CallStack = [pallet_contracts::Frame<Self>; 31];
+    type DepositPerByte = DepositPerByte;
+    type DepositPerItem = DepositPerItem;
+    type DefaultDepositLimit = DefaultDepositLimit;
+    type AddressGenerator = pallet_contracts::DefaultAddressGenerator;
+    type MaxCodeLen = MaxCodeLen;
+    type MaxStorageKeyLen = MaxStorageKeyLen;
+    type UnsafeUnstableInterface = UnsafeUnstableInterface;
+    type MaxDebugBufferLen = MaxDebugBufferLen;
+}
+
 construct_runtime! {
     pub enum Runtime where
         Block = Block,
@@ -1266,6 +1495,9 @@ construct_runtime! {
         DexGeneral: dex_general::{Pallet, Call, Storage, Event<T>} = 101,
         DexStable: dex_stable::{Pallet, Call, Storage, Event<T>}  = 102,
         DexSwapRouter: dex_swap_router::{Pallet, Call, Event<T>} = 103,
+
+        // # smart contracts
+        Contracts: pallet_contracts::{Pallet, Call, Storage, Event<T>} = 110,
     }
 }
 
