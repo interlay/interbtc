9f594882d0f8e7117dcd6fdb2890d1589e6a31cc
Gregory Hill <gregorydhill@outlook.com>
2023-07-10 18:52:33 +0000

    chore: bump spec_version to 1.24.4
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- parachain/runtime/testnet-kintsugi/src/lib.rs
+++ parachain/runtime/testnet-kintsugi/src/lib.rs
@@ -99,7 +99,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("testnet-kintsugi"),
     impl_name: create_runtime_str!("testnet-kintsugi"),
     authoring_version: 1,
-    spec_version: 1024003,
+    spec_version: 1024004,
     impl_version: 1,
     transaction_version: 4,
     apis: RUNTIME_API_VERSIONS,
