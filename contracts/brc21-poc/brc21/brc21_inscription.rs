6e70bb0e3b8cc1ac296685c895f5e67d9ba8b091
Sander Bosma <sanderbosma@gmail.com>
2023-08-09 19:14:08 +0200

    feat: working mint&redeem in contract

--- contracts/brc21-poc/brc21/brc21_inscription.rs
+++ contracts/brc21-poc/brc21/brc21_inscription.rs
@@ -1,137 +1,151 @@
 use crate::*;
 use ink::prelude::{string::String, vec::Vec};
-use serde::{Deserialize, Deserializer};
-use serde_json::Value;
 
-fn deserialize_quoted_integer<'de, D: Deserializer<'de>>(deserializer: D) -> Result<u64, D::Error> {
-    match Value::deserialize(deserializer)? {
-        Value::String(s) => serde_json::from_str::<u64>(&s).map_err(|_| serde::de::Error::custom("wrong type")),
-        _ => Err(serde::de::Error::custom("wrong type")),
-    }
+pub struct Mint {
+    pub ticker: String,
+    pub amount: u64,
+    pub src: String,
 }
 
-#[derive(serde::Deserialize, Debug, PartialEq, Clone)]
-#[serde(tag = "p", rename = "brc-21")]
-pub struct Brc21Inscription {
-    #[serde(flatten)]
-    pub op: Brc21Operation,
-    pub tick: String,
-}
-
-#[derive(serde::Deserialize, Debug, PartialEq, Clone)]
-#[serde(tag = "op")]
-#[serde(rename_all = "camelCase")]
-pub enum Brc21Operation {
-    Deploy {
-        #[serde(deserialize_with = "deserialize_quoted_integer")]
-        max: u64,
-        src: String,
-        id: String,
-    },
-    Mint {
-        #[serde(deserialize_with = "deserialize_quoted_integer")]
-        #[serde(rename = "amt")]
-        amount: u64,
-        src: String,
-    },
-    Transfer {
-        #[serde(deserialize_with = "deserialize_quoted_integer")]
-        amt: u64,
-    },
-    Redeem {
-        #[serde(deserialize_with = "deserialize_quoted_integer")]
-        #[serde(rename = "amt")]
-        amount: u64,
-        dest: String,
-        acc: String,
-    },
-}
-
-pub fn get_brc21_inscriptions(tx: &bitcoin::compat::rust_bitcoin::Transaction) -> Vec<Brc21Inscription> {
-    Inscription::from_transaction(&tx)
+/// Very rudimentary json parsing. Expects a strict ordering of the fields.
+pub fn parse_mint(input: &str) -> Option<Mint> {
+    let parsed = parse_json(&input).ok()?;
+    let obj = parsed
+        .as_object()?
         .into_iter()
-        .filter_map(|inscription| {
-            let body_bytes = inscription.inscription.into_body().unwrap();
-            serde_json::from_slice::<Brc21Inscription>(&body_bytes).ok()
+        .filter_map(|(field, value)| {
+            Some((
+                field.iter().cloned().collect::<String>(),
+                value.as_string()?.iter().cloned().collect::<String>(),
+            ))
         })
-        .collect()
+        .collect::<Vec<_>>();
+
+    if obj.len() != 5 {
+        return None;
+    }
+
+
+    if &obj[0].0 != "p" || &obj[0].1 != "brc-21" {
+        return None;
+    }
+
+    if &obj[1].0 != "op" || &obj[1].1 != "mint" {
+        return None;
+    }
+
+    if &obj[2].0 != "tick" {
+        return None;
+    }
+    let ticker = obj[2].1.clone();
+
+    if &obj[3].0 != "amt" {
+        return None;
+    }
+    let amount = parse_json(&obj[3].1).ok()?.as_number()?.integer;
+
+    if &obj[4].0 != "src" {
+        return None;
+    }
+    let src = obj[4].1.clone();
+
+    Some(Mint { ticker, amount, src })
+}
+
+pub struct Redeem {
+    pub ticker: String,
+    pub amount: u64,
+    pub dest: String,
+    pub account: AccountId,
+}
+
+/// Very rudimentary json parsing. Expects a strict ordering of the fields
+pub fn parse_redeem(input: &str) -> Option<Redeem> {
+    let parsed = parse_json(&input).ok()?;
+    let obj = parsed
+        .as_object()?
+        .into_iter()
+        .filter_map(|(field, value)| {
+            Some((
+                field.iter().cloned().collect::<String>(),
+                value.as_string()?.iter().cloned().collect::<String>(),
+            ))
+        })
+        .collect::<Vec<_>>();
+
+    if obj.len() != 6 {
+        return None;
+    }
+
+    if &obj[0].0 != "p" || &obj[0].1 != "brc-21" {
+        return None;
+    }
+
+    if &obj[1].0 != "op" || &obj[1].1 != "redeem" {
+        return None;
+    }
+
+    if &obj[2].0 != "tick" {
+        return None;
+    }
+    let ticker = obj[2].1.clone();
+
+    if &obj[3].0 != "amt" {
+        return None;
+    }
+    let amount = parse_json(&obj[3].1).ok()?.as_number()?.integer;
+
+    if &obj[4].0 != "dest" {
+        return None;
+    }
+    let dest = obj[4].1.clone();
+
+    if &obj[5].0 != "acc" {
+        return None;
+    }
+    let account_str = obj[5].1.clone();
+
+    let mut account_bytes = [0u8; 32];
+    hex::decode_to_slice(account_str, &mut account_bytes as &mut [u8]).ok()?;
+    let account = TryFrom::try_from(account_bytes).ok()?;
+
+    Some(Redeem {
+        ticker,
+        amount,
+        dest,
+        account,
+    })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::ord::Inscription;
-    use bitcoin::parser::parse_transaction;
-
-    #[test]
-    fn test_inscription() {
-        // txid b61b0172d95e266c18aea0c624db987e971a5d6d4ebc2aaed85da4642d635735
-        let raw_tx = "01000000000101ace8423f874c95f5f9042d7cda6b9f0727251f3059ef827f373a56831cc621a30000000000fdffffff01102700000000000022512037679ea62eab55ebfd442c53c4ad46b6b75e45d8a8fa9cb31a87d0df268b029a03406c00eb3c4d35fedd257051333b4ca81d1a25a37a9af4891f1fec2869edd56b14180eafbda8851d63138a724c9b15384bc5f0536de658bd294d426a36212e6f08a5209e2849b90a2353691fccedd467215c88eec89a5d0dcf468e6cf37abed344d746ac0063036f7264010118746578742f706c61696e3b636861727365743d7574662d38004c5e7b200a20202270223a20226272632d3230222c0a2020226f70223a20226465706c6f79222c0a2020227469636b223a20226f726469222c0a2020226d6178223a20223231303030303030222c0a2020226c696d223a202231303030220a7d6821c19e2849b90a2353691fccedd467215c88eec89a5d0dcf468e6cf37abed344d74600000000";
-        let tx_bytes = hex::decode(&raw_tx).unwrap();
-        let interlay_transaction = parse_transaction(&tx_bytes).unwrap();
-
-        let rust_bitcoin_transaction = interlay_transaction.to_rust_bitcoin().unwrap();
-        let inscriptions = Inscription::from_transaction(&rust_bitcoin_transaction);
-        let expected = "{ \n  \"p\": \"brc-20\",\n  \"op\": \"deploy\",\n  \"tick\": \"ordi\",\n  \"max\": \"21000000\",\n  \"lim\": \"1000\"\n}";
-
-        assert_eq!(inscriptions.len(), 1);
-        let body_bytes = inscriptions[0].clone().inscription.into_body().unwrap();
-        let body = std::str::from_utf8(&body_bytes).unwrap();
-        assert_eq!(body, expected);
-    }
-
-    #[test]
-    fn test_parse_transfer() {
-        let s = r#"{"p": "brc-21", "a": "12", "tick": "ticker", "op": "transfer", "amt": "25"}"#;
-        let parsed: Brc21Inscription = serde_json::from_str(s).unwrap();
-        let expected = Brc21Inscription {
-            op: Brc21Operation::Transfer { amt: 25 },
-            tick: "ticker".to_owned(),
-        };
-        assert_eq!(parsed, expected);
-    }
-
-    #[test]
-    fn test_parse_redeem() {
-        let s = r#"{"p": "brc-21", "a": "12", "tick": "ticker", "op": "redeem", "acc": "someAccount", "amt": "10", "dest": "someDest"}"#;
-        let parsed: Brc21Inscription = serde_json::from_str(s).unwrap();
-        let expected = Brc21Inscription {
-            op: Brc21Operation::Redeem {
-                acc: "someAccount".to_owned(),
-                amount: 10,
-                dest: "someDest".to_owned(),
-            },
-            tick: "ticker".to_owned(),
-        };
-        assert_eq!(parsed, expected);
-    }
 
     #[test]
     fn test_parse_mint() {
-        let s = r#"{"p": "brc-21", "a": "12", "tick": "ticker", "op": "mint", "src": "someSource", "amt": "10"}"#;
-        let parsed: Brc21Inscription = serde_json::from_str(s).unwrap();
-        let expected = Brc21Inscription {
-            op: Brc21Operation::Mint {
-                amount: 10,
-                src: "someSource".to_owned(),
-            },
-            tick: "ticker".to_owned(),
-        };
-        assert_eq!(parsed, expected);
+        parse_mint(
+            r#"{
+                "p": "brc-21",
+                "op": "mint",
+                "tick": "INTR",
+                "amt": "100",
+                "src": "INTERLAY"
+            }"#,
+        )
+        .unwrap();
     }
-
     #[test]
-    fn test_parse_deploy() {
-        let s = r#"{"p": "brc-21", "a": "12", "tick": "ticker", "op": "deploy","id":"myId", "src": "someSource", "max": "10"}"#;
-        let parsed: Brc21Inscription = serde_json::from_str(s).unwrap();
-        let expected = Brc21Inscription {
-            op: Brc21Operation::Deploy {
-                id: "myId".to_owned(),
-                max: 10,
-                src: "someSource".to_owned(),
-            },
-            tick: "ticker".to_owned(),
-        };
-        assert_eq!(parsed, expected);
+    fn test_parse_redeem() {
+        parse_redeem(
+            r#"{
+                    "p": "brc-21",
+                    "op": "redeem",
+                    "tick": "INTR",
+                    "amt": "50",
+                    "dest": "INTERLAY",
+                    "acc": "d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
+                }"#,
+        )
+        .unwrap();
     }
 }
--- contracts/brc21-poc/brc21/lib.rs
+++ contracts/brc21-poc/brc21/lib.rs
@@ -1,16 +1,16 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-use bitcoin::{
-    compat::ConvertFromInterlayBitcoin,
-    types::{FullTransactionProof, Transaction as InterlayTransaction},
-};
-
-use brc21_inscription::{get_brc21_inscriptions, Brc21Inscription, Brc21Operation};
-use ink::{env::Environment, prelude::vec::Vec};
-use ord::Inscription;
-
 mod brc21_inscription;
 mod ord;
+
+use bitcoin::{
+    compat::{ConvertFromInterlayBitcoin, ConvertToInterlayBitcoin},
+    types::FullTransactionProof,
+};
+use brc21_inscription::*;
+use ink::{env::Environment, prelude::vec::Vec, primitives::AccountId};
+use lite_json::json_parser::parse_json;
+use ord::Inscription;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -71,13 +71,13 @@ pub trait DoSomethingInRuntime {
 /// 2. Proof BRC21 redeem to this contract and unlock tokens
 #[ink::contract(env = crate::CustomEnvironment)]
 mod brc21 {
-    use crate::brc21_inscription::Brc21Inscription;
-    use ink::{prelude::string::String};
+    use bitcoin::types::H256Le;
+    use ink::prelude::string::String;
 
     use super::*;
 
     #[ink(event)]
-    pub struct Mint {
+    pub struct MintEvent {
         /// Token ticker
         ticker: String,
         /// Token amount
@@ -91,7 +91,8 @@ mod brc21 {
     }
 
     #[ink(event)]
-    pub struct Redeem {
+    #[cfg_attr(test, derive(PartialEq, Debug))]
+    pub struct RedeemEvent {
         /// Token ticker
         ticker: String,
         /// Token amount
@@ -102,6 +103,8 @@ mod brc21 {
                              * TODO: add to event
                              * #[ink(topic)]
                              * redeem_tx_id: Vec<u8> */
+
+        txid: H256Le,
     }
 
     #[ink(storage)]
@@ -139,34 +142,13 @@ mod brc21 {
         /// - Ensure that the inscription locks the same amount of tokens
         /// - Ensure that the source chain is "INTERLAY"
         #[ink(message, payable)]
-        pub fn mint(&mut self, full_proof: FullTransactionProof) {
+        pub fn mint(&mut self, full_proof: Vec<u8>) {
+            use scale::Decode;
+            let full_proof: FullTransactionProof = Decode::decode(&mut &full_proof[..]).unwrap();
             let is_included = self.env().extension().verify_inclusion(&full_proof).unwrap();
             assert!(is_included);
 
-            let tx = full_proof.user_tx_proof.transaction.to_rust_bitcoin().unwrap();
-
-            let brc21_inscriptions = get_brc21_inscriptions(&tx);
-
-            for inscription in brc21_inscriptions {
-                if let Brc21Inscription {
-                    op: Brc21Operation::Mint { amount, src },
-                    tick,
-                } = inscription
-                {
-                    if tick != self.ticker || src != "INTERLAY" {
-                        continue;
-                    }
-
-                    self.locked += amount as u128;
-
-                    self.env().emit_event(Mint {
-                        ticker: self.ticker.clone(),
-                        amount: amount as u128,
-                        account: self.env().caller(),
-                        // inscription_tx_id: Vec::new(),
-                    });
-                }
-            }
+            self.do_mint(self.env().caller(), full_proof)
         }
 
         /// Unlock tokens to an account and decrease their lock amount
@@ -174,37 +156,61 @@ mod brc21 {
         /// TODO: add the inscription parsing
         /// TODO: add the BTC relay arguments
         #[ink(message, payable)]
-        pub fn redeem(&mut self, account: AccountId, full_proof: FullTransactionProof) {
+        pub fn redeem(&mut self, full_proof: Vec<u8>) {
+            use scale::Decode;
+            let full_proof: FullTransactionProof = Decode::decode(&mut &full_proof[..]).unwrap();
             let is_included = self.env().extension().verify_inclusion(&full_proof).unwrap();
             assert!(is_included);
 
+            self.do_redeem(full_proof)
+        }
+
+        pub fn do_mint(&mut self, caller: AccountId, full_proof: FullTransactionProof) {
             let tx = full_proof.user_tx_proof.transaction.to_rust_bitcoin().unwrap();
 
-            let brc21_inscriptions = get_brc21_inscriptions(&tx);
-
-            for inscription in brc21_inscriptions {
-                if let Brc21Inscription {
-                    op: Brc21Operation::Redeem { amount, dest, acc },
-                    tick,
-                } = inscription
-                {
-                    if tick != self.ticker || dest != "INTERLAY" {
+            for inscription in Inscription::from_transaction(&tx) {
+                let body_bytes = inscription.inscription.into_body().unwrap();
+                let body_str = core::str::from_utf8(&body_bytes).unwrap();
+                if let Some(mint) = parse_mint(body_str) {
+                    if mint.ticker != self.ticker || mint.src != "INTERLAY" {
                         continue;
                     }
 
-                    let mut account_bytes = [0u8; 32];
-                    if hex::decode_to_slice(acc, &mut account_bytes as &mut [u8]).is_ok() {
-                        if let Ok(account) = TryFrom::try_from(account_bytes) {
-                            assert!(self.locked >= amount as u128, "not enough locked tokens");
+                    self.locked += mint.amount as u128;
 
-                            self.env().emit_event(Redeem {
-                                ticker: self.ticker.clone(),
-                                amount: amount as u128,
-                                account,
-                                // redeem_tx_id: Vec::new(),
-                            });
-                        }
+                    self.env().emit_event(MintEvent {
+                        ticker: self.ticker.clone(),
+                        amount: mint.amount as u128,
+                        account: caller,
+                        // inscription_tx_id: Vec::new(),
+                    });
+                }
+            }
+        }
+
+        pub fn do_redeem(&mut self, full_proof: FullTransactionProof) {
+            let tx = full_proof.user_tx_proof.transaction.to_rust_bitcoin().unwrap();
+
+            for inscription in Inscription::from_transaction(&tx) {
+                let body_bytes = inscription.inscription.into_body().unwrap();
+                let body_str = core::str::from_utf8(&body_bytes).unwrap();
+                if let Some(redeem) = parse_redeem(body_str) {
+                    if redeem.ticker != self.ticker || redeem.dest != "INTERLAY" {
+                        continue;
                     }
+
+                    self.locked -= redeem.amount as u128;
+
+                    // calculate txid. We could have calculated this directly on the interlay
+                    // type, but here we use to_interlay just to show how it works
+                    let txid = tx.txid().to_interlay().unwrap();
+
+                    self.env().emit_event(RedeemEvent {
+                        ticker: self.ticker.clone(),
+                        amount: redeem.amount as u128,
+                        account: redeem.account,
+                        txid,
+                    });
                 }
             }
         }
@@ -231,7 +237,7 @@ mod brc21 {
         fn assert_mint_event(event: &ink::env::test::EmittedEvent, ticker: &str, amount: u128, account: AccountId) {
             let decoded_event = decode_event(event);
             match decoded_event {
-                Event::Mint(mint) => {
+                Event::MintEvent(mint) => {
                     assert_eq!(mint.ticker, ticker);
                     assert_eq!(mint.amount, amount);
                     assert_eq!(mint.account, account);
@@ -241,16 +247,103 @@ mod brc21 {
         }
 
         /// Helper function to for redeem event tests
-        fn assert_redeem_event(event: &ink::env::test::EmittedEvent, ticker: &str, amount: u128, account: AccountId) {
+        fn assert_redeem_event(
+            event: &ink::env::test::EmittedEvent,
+            ticker: &str,
+            amount: u128,
+            account: AccountId,
+            txid: H256Le,
+        ) {
             let decoded_event = decode_event(event);
             match decoded_event {
-                Event::Redeem(redeem) => {
-                    assert_eq!(redeem.ticker, ticker);
-                    assert_eq!(redeem.amount, amount);
-                    assert_eq!(redeem.account, account);
+                Event::RedeemEvent(redeem) => {
+                    assert_eq!(
+                        redeem,
+                        RedeemEvent {
+                            account,
+                            amount,
+                            ticker: ticker.to_owned(),
+                            txid
+                        }
+                    );
                 }
                 _ => panic!("Expected Redeem event"),
             }
+        }
+
+        fn alice() -> AccountId {
+            let mut account_bytes = [0u8; 32];
+            hex::decode_to_slice(
+                "d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d",
+                &mut account_bytes as &mut [u8],
+            )
+            .unwrap();
+            let alice = TryFrom::try_from(account_bytes).unwrap();
+            return alice;
+        }
+
+        #[test]
+        fn mint_works() {
+            // a proof of an inscription with the following content:
+            // {
+            //     "p": "brc-21",
+            //     "op": "mint",
+            //     "tick": "INTR",
+            //     "amt": "102",
+            //     "src": "INTERLAY"
+            // }
+
+            let input_hex = "010000000400a5430a02f7abf2a041b6f095c50251b1a9cd0d0823b8fb4f019513032708ab2d0000000000fdffffff0c010100fe899490fd1e1e8fdfca3eec4bc6c50797a4d48b838f6f411857317a03dbbc37d778bc3f63a3c0e9004b2b8c0110b826dc9ac450ed7cfabbfa1a2e0bc1acf8910220bcb3dcca07be4f0adca39637f8bdb4c834c949dba8e82477deaefca09ac6000bac0063036f72640101106170706c69636174696f6e2f6a736f6e004c657b0a202020202270223a20226272632d3231222c0a20202020226f70223a20226d696e74222c0a20202020227469636b223a2022494e5452222c0a2020202022616d74223a2022313032222c0a2020202022737263223a2022494e5445524c4159220a7d0a6884c1bcb3dcca07be4f0adca39637f8bdb4c834c949dba8e82477deaefca09ac6000b041027000000000000885120acc3f6b76c66989933a3a6b8dde9e433c814231bed5e36ff652cf04604d5d9d301000000006901000097f880f50bd19381336deb0adc53a6d73c550ee517c2d0d7f5b508e6ca12d3580000000000000000000000000000000000000000000000000000000000ffff7fb99ad36400000020ce9de6e0e014e8a99d90c3acdb92eff1833dd6e0e95c5b30556e0fdde17418664e2c5de484c3477fba8dc90ce5d50bd537578dd665b94e172ae9a1aa4ff7dd3f020000002001000101000000000300000008e31fdb6f7807dd0f626cfe1eb34c6ae87d9611c2b6d67885161fa6e779be674ff9bf1343bbc1fbe67776b0f2644a568c67538fbf74fdd565da224896d9640914020000000401018e0500000400ffffffff04800000000000000000000000000000000000000000000000000000000000000000083404950000000000885120c233338cda51f9daa2478ae991fe6d9ebd417be058624c745d5a25468d3aeede0000000000000000986a24aa21a9ede0554045adb9e0a0ee9cee26514a3b8f73e3fa31f5fa4cd561d30e41253d90900100000000b500000097f880f50bd19381336deb0adc53a6d73c550ee517c2d0d7f5b508e6ca12d3580000000000000000000000000000000000000000000000000000000000ffff7fb99ad36400000020ce9de6e0e014e8a99d90c3acdb92eff1833dd6e0e95c5b30556e0fdde17418664e2c5de484c3477fba8dc90ce5d50bd537578dd665b94e172ae9a1aa4ff7dd3f02000000200101010000000000030000000c4347c48408493a7778708bb267b5a4ca0bc0d36a6033a8ee489732e7df082b48a5430a02f7abf2a041b6f095c50251b1a9cd0d0823b8fb4f019513032708ab2d3a00325e46f115cf5cd854235180814e5c0bdbee65f968338c13a35436605412";
+            let input_bytes = hex::decode(input_hex).unwrap();
+            use scale::Decode;
+            let full_proof: FullTransactionProof = Decode::decode(&mut &input_bytes[..]).unwrap();
+
+            let mut contract = Brc21 {
+                locked: 200,
+                ticker: "INTR".to_owned(),
+            };
+
+            // call the inner function - this means we skip the inclusion check
+            contract.do_mint(alice(), full_proof);
+
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+
+            assert_mint_event(&emitted_events[0], DEFAULT_TICKER, 102, alice());
+
+            assert_eq!(contract.locked, 302); // starts at 200, mint 102
+        }
+
+        #[test]
+        fn redeem_works() {
+            // a proof of an inscription with the following content:
+            // {
+            //     "p": "brc-21",
+            //     "op": "redeem",
+            //     "tick": "INTR",
+            //     "amt": "50",
+            //     "dest": "INTERLAY",
+            //     "acc": "d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
+            // }
+            // where acc is the Alice account
+
+            let input_hex = "01000000040083511c58b63e49a5043e33430d16a8860166c23ed2598fb10f32138c985c7ac30000000000fdffffff0c0101b1936c1a4fb178cef726c8408a569dd2372031dc4a83d7660b0f88de54be1346560d51d8a9eb7e64f4af245026075f7c3ecc108b834389efd16f20b2e09671d2d503205ea6c870f8b68109a17f15e4219b8bd466d7a43a24082f18c389abc023b2b783ac0063036f72640101106170706c69636174696f6e2f6a736f6e004cb67b0a202020202270223a20226272632d3231222c0a20202020226f70223a202272656465656d222c0a20202020227469636b223a2022494e5452222c0a2020202022616d74223a20223530222c0a202020202264657374223a2022494e5445524c4159222c0a2020202022616363223a202264343335393363373135666464333163363131343161626430346139396664363832326338353538383534636364653339613536383465376135366461323764220a7d0a6884c05ea6c870f8b68109a17f15e4219b8bd466d7a43a24082f18c389abc023b2b783041027000000000000885120343917c817799b69162ba765a2ed311711ca167a552645b805a9e146ff24a9500100000000ba0100008f207ad3026916f8e76fac3421bc01d639c1d5b4ccad455d69cc88b48819b0550000000000000000000000000000000000000000000000000000000000ffff7fcd9ad36400000020e19d7898eca1ee98fbaa79303201893c655a4c37f4271a327b995f58f9cdb9688f338340c560a04f1635a10800feb2e132ef5b7cdd7f26318228d29af01639120300000020010001010000000003000000089dcb4fc367e38c21334e337a7fc9c808e068ebc5a0166d202fcf93fff6f032282f8108552a3e0af701a40f154037dcebfbf4d02336be284de852cc6839ca22b102000000040101f30500000400ffffffff0480000000000000000000000000000000000000000000000000000000000000000008cb824a0000000000885120f519acb8178c569861300691593a04fd6ede802812cbbb5bbca84748846e57570000000000000000986a24aa21a9ed83283d48beef37541d874d8638df44be1389199eebf72dc9b6dd5b44241a41810100000000b50000008f207ad3026916f8e76fac3421bc01d639c1d5b4ccad455d69cc88b48819b0550000000000000000000000000000000000000000000000000000000000ffff7fcd9ad36400000020e19d7898eca1ee98fbaa79303201893c655a4c37f4271a327b995f58f9cdb9688f338340c560a04f1635a10800feb2e132ef5b7cdd7f26318228d29af016391203000000200101010000000000030000000cca2e0f7d98990102eee51e63364b2de57cab1b44a507cb16db97f5ec23fa895483511c58b63e49a5043e33430d16a8860166c23ed2598fb10f32138c985c7ac3df11510fef13b96d681f58433ac02f99fd917eeacbdd833855718722a7aad9c1";
+            let input_bytes = hex::decode(input_hex).unwrap();
+            use scale::Decode;
+            let full_proof: FullTransactionProof = Decode::decode(&mut &input_bytes[..]).unwrap();
+
+            let mut contract = Brc21 {
+                locked: 200,
+                ticker: "INTR".to_owned(),
+            };
+
+            // call the inner function - this means we skip the inclusion check
+            contract.do_redeem(full_proof.clone());
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+
+            let txid = full_proof.user_tx_proof.transaction.tx_id_bounded(u32::MAX).unwrap();
+            assert_redeem_event(&emitted_events[0], DEFAULT_TICKER, 50, alice(), txid);
+
+            assert_eq!(contract.locked, 150); // starts at 200, redeemed 50
         }
 
         /// Test if the default constructor does its job.
@@ -260,74 +353,6 @@ mod brc21 {
             assert_eq!(brc21.get_ticker(), DEFAULT_TICKER);
             assert_eq!(brc21.get_locked(), 0);
         }
-
-//         /// Test if minting works
-//         #[ink::test]
-//         fn mint_works() {
-//             let mut brc21 = Brc21::new(DEFAULT_TICKER.to_string());
-// 
-//             // Load the default accounts
-//             let accounts = ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
-// 
-//             // Alice mints 100 coins
-//             // Default caller is the Alice account 0x01
-//             brc21.mint(100);
-//             assert_eq!(brc21.get_locked(), 100);
-// 
-//             // Check that the event was emitted
-//             let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
-//             assert_eq!(emitted_events.len(), 1);
-//             assert_mint_event(
-//                 &emitted_events[0],
-//                 DEFAULT_TICKER,
-//                 100,
-//                 AccountId::from([0x01; 32]), // Alice
-//             );
-// 
-//             // Bob mints 50 coins
-//             ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
-//             brc21.mint(50);
-//             assert_eq!(brc21.get_locked(), 150);
-// 
-//             // Check that the event was emitted
-//             let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
-//             assert_eq!(emitted_events.len(), 2);
-//             assert_mint_event(
-//                 &emitted_events[1],
-//                 DEFAULT_TICKER,
-//                 50,
-//                 AccountId::from([0x02; 32]), // Bob
-//             );
-//         }
-// 
-//         /// Test if redeeming works
-//         #[ink::test]
-//         fn redeem_works() {
-//             let mut brc21 = Brc21::new(DEFAULT_TICKER.to_string());
-// 
-//             // Load the default accounts
-//             let accounts = ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
-// 
-//             // Alice mints 100 coins
-//             // Default caller is the Alice account 0x01
-//             brc21.mint(100);
-//             assert_eq!(brc21.get_locked(), 100);
-// 
-//             // Bob redeems 50 coins
-//             ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
-//             brc21.redeem(accounts.bob, 50);
-//             assert_eq!(brc21.get_locked(), 50);
-// 
-//             // Check that the event was emitted
-//             let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
-//             assert_eq!(emitted_events.len(), 2);
-//             assert_redeem_event(
-//                 &emitted_events[1],
-//                 DEFAULT_TICKER,
-//                 50,
-//                 AccountId::from([0x02; 32]), // Bob
-//             );
-//         }
     }
 
     /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
