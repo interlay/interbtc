d94c71c401d3b11cb3a39cda7c2c189cbaf43a6a
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-10-03 14:21:09 +0200

    chore: Update runtimes with forked ORML Xtokens

--- parachain/runtime/kintsugi/src/xcm_config.rs
+++ parachain/runtime/kintsugi/src/xcm_config.rs
@@ -6,10 +6,9 @@ use frame_support::{
     traits::{Everything, Get, Nothing},
 };
 use orml_asset_registry::{AssetRegistryTrader, FixedRateAssetRegistryTrader};
-use orml_traits::{
-    location::AbsoluteReserveProvider, parameter_type_with_key, FixedConversionRateProvider, MultiCurrency,
-};
+use orml_traits::{parameter_type_with_key, FixedConversionRateProvider, MultiCurrency};
 use orml_xcm_support::{DepositToAlternative, IsNativeConcrete, MultiCurrencyAdapter, MultiNativeAsset};
+use orml_xtokens::AbsoluteReserveProviderMigrationPhase;
 use pallet_xcm::XcmPassthrough;
 use polkadot_parachain::primitives::Sibling;
 use runtime_common::Transactless;
@@ -192,7 +191,7 @@ impl xcm_executor::Config for XcmConfig {
     #[cfg(not(feature = "runtime-benchmarks"))]
     type AssetTransactor = LocalAssetTransactor;
     type OriginConverter = XcmOriginToTransactDispatchOrigin;
-    type IsReserve = MultiNativeAsset<AbsoluteReserveProvider>;
+    type IsReserve = MultiNativeAsset<AbsoluteReserveProviderMigrationPhase<Runtime>>;
     type IsTeleporter = Nothing; // no teleportation allowed
     type Barrier = Barrier;
     type Weigher = FixedWeightBounds<UnitWeightCost, RuntimeCall, MaxInstructions>;
@@ -426,8 +425,9 @@ impl orml_xtokens::Config for Runtime {
     type MaxAssetsForTransfer = MaxAssetsForTransfer;
     type MinXcmFee = ParachainMinFee;
     type MultiLocationsFilter = Everything;
-    type ReserveProvider = AbsoluteReserveProvider;
+    type ReserveProvider = AbsoluteReserveProviderMigrationPhase<Runtime>;
     type UniversalLocation = UniversalLocation;
+    type MigrationPhaseUpdateOrigin = EnsureRoot<AccountId>;
 }
 
 #[cfg(feature = "runtime-benchmarks")]
